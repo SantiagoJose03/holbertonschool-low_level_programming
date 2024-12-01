2bc39aa7cf545c5cf3b38d104a8c198cefd40337
santiagojose52803 <santiagojose52803@gmail.com>
2024-12-01 02:04:55 -0400

    randome number

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,10 +1,33 @@
-#!/bin/bash
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
 
+	/* Initialize random number generator */
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/*  */
+
+	/* Print whether the number is positive, zero, or negative */
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+
 	return (0);
 }
