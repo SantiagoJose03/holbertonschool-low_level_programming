267d126131cf5c9a72917083c275f3bbdc91ec1e
santiagojose52803 <santiagojose52803@gmail.com>
2024-12-01 02:19:48 -0400

    reverse

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+
+Here’s the program that prints the lowercase alphabet in reverse using only putchar twice:
+
+c
+Copy code
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+
+	return (0);
+}
